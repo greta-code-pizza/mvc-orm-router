2d13de611adbb4447b2034c8fb29162140b878d1
Nicolas Le Chenic <nicolaslechenic@gmail.com>
2019-03-17 22:08:44 +0100

    update dump database

--- dbproject.sql
+++ dbproject.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Mar 11, 2019 at 01:45 PM
+-- Generation Time: Mar 17, 2019 at 09:08 PM
 -- Server version: 5.7.23
 -- PHP Version: 7.2.10
 
@@ -26,6 +26,12 @@ CREATE TABLE `articles` (
   `content` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `articles`
+--
+
+INSERT INTO `articles` (`id`, `title`, `content`) VALUES
+(88, 'aeeza', 'zaeeaz');
 
 -- --------------------------------------------------------
 
@@ -51,6 +57,26 @@ INSERT INTO `events` (`id`, `start`, `end`, `title`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `join_articles_tags`
+--
+
+CREATE TABLE `join_articles_tags` (
+  `id` mediumint(9) NOT NULL,
+  `id_article` mediumint(9) NOT NULL,
+  `id_tag` mediumint(9) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `join_articles_tags`
+--
+
+INSERT INTO `join_articles_tags` (`id`, `id_article`, `id_tag`) VALUES
+(32, 88, 4),
+(33, 88, 6);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tags`
 --
 
@@ -59,18 +85,16 @@ CREATE TABLE `tags` (
   `label` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `join_events_tags`
+-- Dumping data for table `tags`
 --
 
-CREATE TABLE `join_articles_tags` (
-  `id` mediumint(9) NOT NULL,
-  `id_article` int NOT NULL ,
-  `id_tag` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
+INSERT INTO `tags` (`id`, `label`) VALUES
+(4, 'web'),
+(5, 'ruby'),
+(6, 'php'),
+(7, 'javascript'),
+(8, 'blockchain');
 
 --
 -- Indexes for dumped tables
@@ -89,15 +113,17 @@ ALTER TABLE `events`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `join_articles_tags`
+--
+ALTER TABLE `join_articles_tags`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_article_fk` (`id_article`),
+  ADD KEY `id_tag_fk` (`id_tag`);
+
+--
 -- Indexes for table `tags`
 --
 ALTER TABLE `tags`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `join_events_tags`
---
-ALTER TABLE `join_articles_tags`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -108,7 +134,7 @@ ALTER TABLE `join_articles_tags`
 -- AUTO_INCREMENT for table `articles`
 --
 ALTER TABLE `articles`
-  MODIFY `id` mediumint(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` mediumint(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=89;
 
 --
 -- AUTO_INCREMENT for table `events`
@@ -117,29 +143,24 @@ ALTER TABLE `events`
   MODIFY `id` mediumint(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `tags`
---
-ALTER TABLE `tags`
-  MODIFY `id` mediumint(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
 -- AUTO_INCREMENT for table `join_articles_tags`
 --
 ALTER TABLE `join_articles_tags`
-  MODIFY `id` mediumint(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` mediumint(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
--- CREATE TABLE `articles` (
---   `id` mediumint(9) NOT NULL,
---   `title` varchar(255) NOT NULL,
---   `content` text NOT NULL
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+--
+-- AUTO_INCREMENT for table `tags`
+--
+ALTER TABLE `tags`
+  MODIFY `id` mediumint(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
+--
+-- Constraints for dumped tables
+--
 
--- CREATE TABLE join_articles_tags (
---   id mediumint(9) NOT NULL,
---   id_article int NOT NULL ,
---   id_tag  int NOT NULL,
---   CONSTRAINT join_articles_tags_fk PRIMARY KEY (id),
---   CONSTRAINT articles_fk FOREIGN KEY (id_article) REFERENCES articles(id),
---   CONSTRAINT tags_fk FOREIGN KEY (id_tag) REFERENCES tags(id)
--- )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+--
+-- Constraints for table `join_articles_tags`
+--
+ALTER TABLE `join_articles_tags`
+  ADD CONSTRAINT `id_article_fk` FOREIGN KEY (`id_article`) REFERENCES `articles` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `id_tag_fk` FOREIGN KEY (`id_tag`) REFERENCES `tags` (`id`);
